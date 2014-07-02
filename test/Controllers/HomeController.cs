fefc5438ca679a304b7c3d8a2d511c8daf53c8a3
Fatumner <Fatumner@wp.pl>
2014-07-02 13:20:08 +0200

    dodanie modelu i tabeli na podstawie danych z bd

--- test/Controllers/HomeController.cs
+++ test/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CountryAndLanguageModel;
 
 namespace test.Controllers
 {
     public class HomeController : Controller
     {
+        private CountryAndLanguageModel.CountryAndLanguageEntities db = new CountryAndLanguageModel.CountryAndLanguageEntities();
         // GET: Home
         public ActionResult Index()
         {
@@ -15,7 +17,27 @@ namespace test.Controllers
         }
         public ActionResult countries()
         {
-            return View();
+            List<Country> Countries = db.Countries.ToList();
+            List<Countrylanguage> CountryLanguages = db.Countrylanguages.ToList();
+            
+            Models.CountriesAndLanguages CAL = new Models.CountriesAndLanguages();
+
+            CAL.Countries = (from Country in Countries
+                             join Countrylanguage in CountryLanguages on Country.Code equals Countrylanguage.Countrycode
+                             where Countrylanguage.Isofficial == true
+                             orderby Country.Continent ascending,
+                                     Country.Name ascending,
+                                     Countrylanguage.Percentage descending
+                             select Country).ToList();
+            CAL.CountryLanguages = (from Country in Countries
+                                    join Countrylanguage in CountryLanguages on Country.Code equals Countrylanguage.Countrycode
+                                    where Countrylanguage.Isofficial == true
+                                    orderby Country.Continent ascending,
+                                            Country.Name ascending,
+                                            Countrylanguage.Percentage descending
+                                    select Countrylanguage).ToList();
+
+            return View(CAL);
         }
         public ActionResult groupedCountries()
         {
---
+++ test/Models/CountriesAndLanguages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public class CountriesAndLanguages
+    {
+        public List<CountryAndLanguageModel.Country> Countries { get; set; }
+        public List<CountryAndLanguageModel.Countrylanguage> CountryLanguages { get; set; }
+    }
+}
