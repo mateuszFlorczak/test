efb1a83013be4730c86748408c82486ce3352c5a
Fatumner <Fatumner@wp.pl>
2014-07-02 17:12:29 +0200

    few fixes

--- test/Controllers/HomeController.cs
+++ test/Controllers/HomeController.cs
@@ -43,11 +43,46 @@ namespace test.Controllers
         }
         public ActionResult groupedCountries()
         {
-            return View();
+            List<countrylanguage> countryLanguages = db.countrylanguage.ToList();
+            List<country> Countries = db.country.ToList();
+
+            List<Models.WorldLanguages> WL = new List<Models.WorldLanguages>();
+            long population = 0;
+            foreach (var countr in Countries)
+            {
+                population += countr.population;
+            }
+
+            WL = (from Countrylanguage in countryLanguages
+                  join Country in Countries on Countrylanguage.countrycode equals Country.code
+                  group new { Countrylanguage, Country } by Countrylanguage.language into Languages
+                  select new Models.WorldLanguages
+                  {
+                      LanguageName = Languages.Key,
+                      Population = Languages.Sum(x => x.Country.population),
+                      Percentage = 1//(float)Math.Round((double)(Languages.Sum(x => x.Country.Population) / population), 2)
+                  }).OrderByDescending((x => x.Population)).ThenBy(x => x.LanguageName).ToList();
+
+            return View(WL);
         }
         public ActionResult country(string CountryCode)
         {
-            return View();
+            if (CountryCode == null)
+            {
+
+            }
+            else if (CountryCode.Count() != 3)
+            {
+
+            }
+            country country = (from Country in db.country
+                               where Country.code == CountryCode
+                               select Country).First();
+            ViewBag.CountryName = country.name;
+            List<countrylanguage> countryLanguages = (from CountryLanguage in db.countrylanguage
+                                                      where CountryLanguage.countrycode == CountryCode
+                                                      select CountryLanguage).ToList();
+            return View(countryLanguages);
         }
     }
 }
---
+++ test/Models/WorldLanguages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public class WorldLanguages
+    {
+        public string LanguageName { get; set; }
+        public int Population { get; set; }
+        public float Percentage { get; set; }
+    }
+}
